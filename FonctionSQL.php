f8b68c1b6a98383e89237937d5c66310d0e38276
diayaya1999@gmail.com <diayaya1999@gmail.com>
2024-09-04 11:46:46 +0000

    les fonctions en SQL

---
+++ FonctionSQL.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fonctions , filtres et expressions régulières SQL</title>
+</head>
+<body>
+    <?php
+     $serveur = "localhost";
+     $login = "root";
+     $pass =  "dia_yaya";
+
+     try{
+     $connexion = new PDO("mysql:host=$serveur;dbname=tachephp", $login, $pass);
+     $connexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $foncsql = "SELECT AVG (age) FROM incrits";
+    $requete = $connexion->prepare( $foncsql);
+    $requete->execute();
+
+    $resultat = $requete->fetchAll();
+    echo '<pre>';
+    print_r($resultat);
+    echo '</pre>';
+     }
+    
+
+     catch(PDOException $e){
+        echo 'Echec de la connexion : ' .$e->getMessage();
+     }
+    ?>
+</body>
+</html>
