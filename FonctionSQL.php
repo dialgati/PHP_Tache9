1d5619f15846fc5d06e70f7f4d0a29e22fcf9ddb
diayaya1999@gmail.com <diayaya1999@gmail.com>
2024-09-04 11:48:23 +0000

    la fonction COUNT

--- FonctionSQL.php
+++ FonctionSQL.php
@@ -15,7 +15,7 @@
      $connexion = new PDO("mysql:host=$serveur;dbname=tachephp", $login, $pass);
      $connexion -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $foncsql = "SELECT AVG (age) FROM incrits";
+    $foncsql = "SELECT COUNT(*) FROM table";
     $requete = $connexion->prepare( $foncsql);
     $requete->execute();
 
